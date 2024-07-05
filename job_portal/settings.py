5e096f789b7d9bf0ab218dad1f69e7cfcf1b72c4
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-07-05 17:36:48 +0530

    added allowd host

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -12,10 +12,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+import environ
 
-# Load environment variables from .env file
-load_dotenv()
+# Initialize environment variables
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Initialize environment variables
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Reading .env file
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +38,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['manjakaramal.pythonanywhere.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+print(f"SECRET_KEY: {SECRET_KEY}")
+print(f"DEBUG: {DEBUG}")
+print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
 
 # Application definition
