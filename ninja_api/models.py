b9bf2480b6fe75a43f3bdefd0c576ce15f786cbd
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-07-06 13:38:20 +0530

    in models maxlength from charfield increesed to 500 and address charfield change to textfield

--- ninja_api/models.py
+++ ninja_api/models.py
@@ -10,66 +10,66 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='users', null=True, blank=True)
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
-    image_url = models.CharField(max_length=150, null=True, blank=True)
+    name = models.CharField(max_length=500)
+    image_url = models.CharField(max_length=500, null=True, blank=True)
     def __str__(self):
         return self.name
 
 class SubCategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 class ManyCategory(models.Model):
     sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 class Country(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 class State(models.Model):
     country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 class District(models.Model):
     state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
     
 class Keyword(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 class CompanyType(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 class Company(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=500)
     location = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, blank=True)
-    address = models.CharField(max_length=150, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
     company_type = models.ForeignKey(CompanyType, on_delete=models.SET_NULL, null=True, blank=True)
-    website = models.CharField(max_length=150, null=True, blank=True)
-    instagram = models.CharField(max_length=150, null=True, blank=True)
-    facebook = models.CharField(max_length=150, null=True, blank=True)
-    google_map = models.CharField(max_length=150, null=True, blank=True)
+    website = models.CharField(max_length=500, null=True, blank=True)
+    instagram = models.CharField(max_length=500, null=True, blank=True)
+    facebook = models.CharField(max_length=500, null=True, blank=True)
+    google_map = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -87,9 +87,9 @@ class Job(models.Model):
     many_category = models.ManyToManyField(ManyCategory, blank=True)
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
     location = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, blank=True)
-    address = models.CharField(max_length=150, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
     keyword = models.ForeignKey(Keyword, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=500)
     expirience = models.ForeignKey(Expirience, on_delete=models.SET_NULL, null=True, blank=True)
     job_type = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -100,7 +100,7 @@ class Job(models.Model):
     phone_two = models.CharField(max_length=20, null=True, blank=True)
     phone_three = models.CharField(max_length=20, null=True, blank=True)
     whatsapp = models.CharField(max_length=20, null=True, blank=True)
-    source = models.CharField(max_length=150, null=True, blank=True)
+    source = models.CharField(max_length=500, null=True, blank=True)
     posted = models.DateField(auto_now_add=True, null=True)
     closing_date = models.DateField(null=True, blank=True)
 
