dce304b9b3af2267f9286bd643e1b195ed0495a4
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-07-09 21:43:35 +0530

    added /api/jobs/search?query=developer

--- ninja_api/api.py
+++ ninja_api/api.py
@@ -1,8 +1,9 @@
-from ninja import Router
+from ninja import Router, Query
 from .models import Category, SubCategory, Job
 from .schemas import CategorySchema, SubCategorySchema, JobSchema
-from typing import List
+from typing import List, Optional
 from ninja.pagination import paginate, PageNumberPagination
+from django.db.models import Q
 
 router = Router()
 
@@ -27,7 +28,8 @@ def get_job(request, job_id: int):
 def list_category_subcategories(request, category_id: int):
     subcategories = SubCategory.objects.filter(category_id=category_id)
     return subcategories
-        
+
+# /api/categories/${categoryId}/jobs?sub_category=${subcategoryId}        
 @router.get('/categories/{category_id}/jobs/', response=List[JobSchema])
 def list_category_job(request, category_id: int, sub_category: int = None):
     category = Category.objects.get(id=category_id)
@@ -37,4 +39,40 @@ def list_category_job(request, category_id: int, sub_category: int = None):
         jobs_query = jobs_query.filter(sub_category=sub_category)
     
     jobs = jobs_query.all()
+    return jobs
+
+# @router.get("/jobs/search", response=List[JobSchema])
+# def search_jobs(request, name: Optional[str] = Query(None), category: Optional[int] = Query(None), subcategory: Optional[int] = Query(None)):
+#     jobs = Job.objects.all()
+
+#     if name:
+#         jobs = jobs.filter(name__icontains=name)
+    
+#     if category:
+#         jobs = jobs.filter(category_id=category)
+    
+#     if subcategory:
+#         jobs = jobs.filter(sub_category__id=subcategory)
+    
+#     return jobs
+
+# /api/jobs/search - returns all jobs.
+# /api/jobs/search?name=developer - returns jobs with "developer" in their name.
+# /api/jobs/search?category=1 - returns jobs in category with ID 1.
+# /api/jobs/search?subcategory=2 - returns jobs in subcategory with ID 2.
+# /api/jobs/search?name=developer&category=1&subcategory=2 - returns jobs matching all provided filters.
+
+
+# /api/jobs/search?query=developer
+@router.get("/jobs/search", response=List[JobSchema])
+def search_jobs(request, query: Optional[str] = Query(None)):
+    jobs = Job.objects.all()
+
+    if query:
+        jobs = jobs.filter(
+            Q(name__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(sub_category__name__icontains=query)
+        )
+    
     return jobs
