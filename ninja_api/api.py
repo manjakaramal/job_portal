abc159fdbfd5e18c72678c1f9b1da7aad0ec8cbd
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-07-09 16:03:57 +0530

    added category job

--- ninja_api/api.py
+++ ninja_api/api.py
@@ -11,11 +11,6 @@ def list_categories(request):
     categories = Category.objects.all()
     return categories
 
-@router.get('/categories/{category_id}/subcategories/', response=List[SubCategorySchema])
-def list_subcategories(request, category_id: int):
-    subcategories = SubCategory.objects.filter(category_id=category_id)
-    return subcategories
-
 
 @router.get('/jobs/', response=List[JobSchema])
 @paginate(PageNumberPagination)
@@ -27,3 +22,19 @@ def list_jobs(request):
 def get_job(request, job_id: int):
     job = Job.objects.get(id=job_id)
     return job
+
+@router.get('/categories/{category_id}/subcategories/', response=List[SubCategorySchema])
+def list_category_subcategories(request, category_id: int):
+    subcategories = SubCategory.objects.filter(category_id=category_id)
+    return subcategories
+        
+@router.get('/categories/{category_id}/jobs/', response=List[JobSchema])
+def list_category_job(request, category_id: int, sub_category: int = None):
+    category = Category.objects.get(id=category_id)
+    jobs_query = Job.objects.filter(category=category)
+    
+    if sub_category:
+        jobs_query = jobs_query.filter(sub_category=sub_category)
+    
+    jobs = jobs_query.all()
+    return jobs
